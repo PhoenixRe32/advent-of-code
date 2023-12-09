e1d636949d999b417f2ef66448877bfb23b0fcb3
Andreas Ανδρέου <andreas.andreou@masabi.com>
2023-12-09 16:35:21 +0000

    Restructure folder

---
+++ src/day01/Day01.kt
@@ -0,0 +1,91 @@
+package day01
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input
+            .map { line ->
+                line.toCharArray()
+            }
+            .map { charArray ->
+                charArray.filter { it.isDigit() }
+            }
+            .map {
+                "${it.first()}${it.last()}".toInt()
+//                    .also(Int::println)
+            }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return input
+            .map { line ->
+                val orderedWordDigits = digitMap
+                    .map { (word, digit) ->
+                        line.indexOf(word) to word
+                    }
+                    .filter { (index, word) -> index != -1 }
+                    .sortedBy { it.first }
+                    .map { it.second }
+                if (orderedWordDigits.isEmpty())
+                    line
+                else if (orderedWordDigits.size == 1) {
+                    val firstWordDigit: String = orderedWordDigits.first()
+                    line.replace(firstWordDigit, digitMap[firstWordDigit].toString(), true)
+                } else {
+                    val firstWordDigit: String = orderedWordDigits.first()
+                    val lastWordDigit: String = orderedWordDigits.last()
+                    line
+                        .replace(firstWordDigit, digitMap[firstWordDigit].toString(), true)
+                        .replace(lastWordDigit, digitMap[lastWordDigit].toString(), true)
+                }
+            }
+            .map { charArray ->
+                charArray.filter { it.isDigit() }
+            }
+            .map {
+                "${it.first()}${it.last()}".toInt()
+//                    .also(Int::println)
+            }
+            .sum()
+    }
+
+    val testInput1 = readInput("day01/test1")
+    check(part1(testInput1).also { println("Test input part 1 result is: $it") } == 142)
+    val testInput2 = readInput("day01/test2")
+    check(part2(testInput2).also { println("Test input part 2 result is: $it") } == 281)
+
+    val input = readInput("day01/input")
+    println("Actual Answer Part 1")
+    println("===========")
+    part1(input).println()
+    println("Actual Answer Part 2 54980")
+    println("===========")
+    part2(input).println()
+}
+
+private val digitMap = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+    "zero" to 0,
+//    "ten" to 10,
+//    "eleven" to 11,
+//    "twelve" to 12,
+//    "thirteen" to 13,
+//    "fourteen" to 14,
+//    "fifteen" to 15,
+//    "sixteen" to 16,
+//    "seventeen" to 17,
+//    "eighteen" to 18,
+//    "nineteen" to 19,
+//    "twenty" to 20,
+)
---
+++ src/day02/Day02.kt
@@ -0,0 +1,95 @@
+package day02
+
+import println
+import readInput
+import kotlin.math.max
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.mapNotNull {
+            val gameSections = it.splitGameSections()
+            val roundResults = gameSections[1]
+                .splitGameRounds()
+                .map(String::toResult)
+            if (roundResults.all(RGB::isPossible)) gameSections[0]
+            else null
+        }
+            .map { it.getId() }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.map {
+            val gameSections = it.splitGameSections()
+            gameSections[1]
+                .splitGameRounds()
+                .map(String::toResult)
+                .foldRight(MinRGB(), ::buildMinRequirement)
+        }
+            .map(MinRGB::power)
+            .sum()
+    }
+
+    val testInput1 = readInput("day02/test1")
+    check(part1(testInput1).also { println("Test input part 1 result is: $it") } == 8)
+
+    val input = readInput("day02/input")
+    println("Actual Answer Part 1")
+    println("===========")
+    part1(input).println()
+
+    val testInput2 = readInput("day02/test2")
+    check(part2(testInput2).also { println("Test input part 2 result is: $it") } == 2286)
+
+    println("Actual Answer Part 2")
+    println("===========")
+    part2(input).println()
+}
+
+private fun String.splitGameSections(): List<String> =
+    split(':')
+
+private fun String.splitGameRounds(): List<String> =
+    split(';')
+
+private fun String.toResult(): RGB =
+    split(',')
+        .map(String::trim)
+        .map(String::lowercase)
+        .foldRight(RGB(), ::buildRoundResult)
+
+private fun buildRoundResult(roundResultDescription: String, roundResult: RGB): RGB =
+    if (roundResultDescription.endsWith("red"))
+        roundResult.copy(red = roundResultDescription.removeSuffix("red").trim().toInt())
+    else if (roundResultDescription.endsWith("blue"))
+        roundResult.copy(blue = roundResultDescription.removeSuffix("blue").trim().toInt())
+    else
+        roundResult.copy(green = roundResultDescription.removeSuffix("green").trim().toInt())
+
+private fun buildMinRequirement(roundResult: RGB, minRequirement: MinRGB): MinRGB =
+    minRequirement.copy(
+        red = max(minRequirement.red, roundResult.red),
+        green = max(minRequirement.green, roundResult.green),
+        blue = max(minRequirement.blue, roundResult.blue),
+    )
+
+private fun String.getId(): Int =
+    lowercase().removePrefix("game").trim().toInt()
+
+private data class RGB(
+    val red: Int = 0,
+    val green: Int = 0,
+    val blue: Int = 0,
+) {
+    val isPossible = red in 0..12
+            && green in 0..13
+            && blue in 0..14
+}
+
+private data class MinRGB(
+    val red: Int = 0,
+    val green: Int = 0,
+    val blue: Int = 0,
+) {
+    val power = red * green * blue
+}
